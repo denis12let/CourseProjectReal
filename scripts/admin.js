3df974068761f1d01a9433efe7abb7b790b7d1fa
denis12let <84den84sokol@gmail.com>
2023-11-19 15:31:42 +0300

    ув

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,6 +1,6 @@
 const addProductClick = document.querySelector("#addClick");
 
-function fetchProduct(productData) {    
+function fetchProduct(productData) {
     fetch("http://localhost:8080/products/add", {
         method: "POST", 
         headers: {
@@ -14,6 +14,7 @@ function fetchProduct(productData) {
     .then(data => {
         console.log(data);
         if (data.message === "Продукт успешно зарегистрирован") {
+
             // location.replace("../index.html")
         } else {
             console.log('Ошибка добавления');
@@ -22,45 +23,33 @@ function fetchProduct(productData) {
     .catch(error => console.log(error));
 }
 
-fetch("http://localhost:8080/products")
+fetch("http://localhost:8080/products/")
   .then(res => res.json())
   .then(data => {
-      const itemContainer = document.querySelector('.items'); // Замените селектор на селектор вашего контейнера, в котором будете отрисовывать карточки товара
-      
-      // Для каждого товара в полученных данных
+      const itemContainer = document.querySelector('.items'); 
       data.forEach(item => {
-          // Создаем разметку карточки товара
           const itemElement = document.createElement('div');
           itemElement.href = "link";
           itemElement.classList.add('item');
           itemElement.innerHTML = `
-              <div class="basket_item_icon">
-                <button class = "image-button">
-                </button>
-              </div>
-              <div class="information_field">
-                  <div class="item_image">
-                      <img src="${item.imageUrl}" alt="" />
-                  </div>
-                  <div class="item_name">
-                    <a href="/pages/product.html?itemId=${item.id}">${item.title}</a>
-                  </div>
-                  <div class="item_price">${item.price} руб.</div>
-              </div>
-              <div class="views">
-                  <div class="rate">
-                      <div class="rate_icon">
-                          <img src="/icons/rate.png" />
-                      </div>
-                      <div class="rate_count">${item.rating}</div>
-                  </div>
-                  <div class="review">
-                      <div class="rev_count">2 отзыва</div>
-                      <div class="review_icon">
-                          <img src="/icons/review.png" />
-                      </div>
-                  </div>
-              </div>
+            <div class="item-cart__image">
+                <img src=${item.imageUrl} alt="">
+            </div>
+            <div class="item-cart__content">
+                <h4 class="item-cart__title">${item.title}</h4>
+                <p class="item-cart__descr">${item.description}</p>
+            </div>
+            <div class="item-cart__counter">
+                <input type="number" id="typeNumber" class="form-control" value="${item.count}"/>
+            </div>
+            <div class="item-cart__price">
+               ${item.price}
+            </div>
+            <div class="item-cart__delete">
+              <button type="button" class="btn-close btn btn-secondary" 
+              aria-label="Close" data-bs-toggle="tooltip" 
+              data-bs-placement="right" title="Удалить"></button>
+            </div>
           `;
             
           itemContainer.appendChild(itemElement); // Добавляем карточку товара в контейнер
@@ -68,7 +57,7 @@ fetch("http://localhost:8080/products")
 });
 
 handleAddProductClick = () => {
-    const url = document.querySelector("#addURL");
+    const imageUrl = document.querySelector("#addURL");
     const title = document.querySelector("#addTitle");
     const description = document.querySelector("#addDescription");
     const price  = document.querySelector("#addPrice");
@@ -76,7 +65,7 @@ handleAddProductClick = () => {
     const rate = document.querySelector("#addRate");
     const source = document.querySelector("#addSource");
     const productData = {
-        url: url.value,
+        imageUrl: imageUrl.value,
         title: title.value,
         description: description.value,
         price: price.value,
@@ -87,4 +76,4 @@ handleAddProductClick = () => {
     fetchProduct(productData);
 }
 
-addProductClick.addEventListener('click', handleAddProductClick());
+addProductClick.addEventListener('click', handleAddProductClick);
